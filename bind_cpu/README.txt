532a01610c81375ef15121fe95496f84f49f9c87
luowei3 <1106581243@qq.com>
2021-03-18 19:49:53 +0800

    linux绑核测试

---
+++ bind_cpu/README.txt
@@ -0,0 +1,2 @@
+linux 线程和进程绑核测试
+https://blog.csdn.net/guotianqing/article/details/80958281
---
+++ bind_cpu/bind_cpu_1.cpp
@@ -0,0 +1,68 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/sysinfo.h>
+#include <unistd.h>
+#include <sched.h>
+#include <ctype.h>
+#include <string.h>
+#include <pthread.h>
+#define THREAD_MAX_NUM 200 // CPU核的最大个数
+int num = 0;               // CPU核的个数
+
+void *thread_Fun(void *arg)
+{
+    int *cpu_idx = (int *)arg;
+    cpu_set_t set_mask; // CPU核的集合
+    cpu_set_t get_mask; // 获取在集合中的CPU
+
+    printf("thread is:%d\n", *cpu_idx);
+    CPU_ZERO(&set_mask);                                         // 置空
+    CPU_SET(*cpu_idx, &set_mask);                                // 设置CPU亲和力
+    if (sched_setaffinity(0, sizeof(set_mask), &set_mask) == -1) //设置线程CPU亲和力
+    {
+        printf("warning: could not set CPU affinity, continuing...\n");
+    }
+
+    CPU_ZERO(&get_mask);
+    if (sched_getaffinity(0, sizeof(get_mask), &get_mask) == -1) //获取线程CPU亲和力
+    {
+        printf("warning: cound not get thread affinity, continuing...\n");
+    }
+
+    for (int i = 0; i < num; i++)
+    {
+        if (CPU_ISSET(i, &get_mask))
+        { // 判断线程与哪个CPU有亲和力
+            printf("this thread %d is running processor : %d\n", *cpu_idx, i);
+        }
+    }
+
+    return NULL;
+}
+
+int main()
+{
+    int tid[THREAD_MAX_NUM];
+    pthread_t thread[THREAD_MAX_NUM];
+
+    num = sysconf(_SC_NPROCESSORS_CONF); //获取CPU核的个数
+    if (num > THREAD_MAX_NUM)
+    {
+        printf("num of cores[%d] is bigger than THREAD_MAX_NUM[%d]!\n", num, THREAD_MAX_NUM);
+        return -1;
+    }
+    printf("system has %i processor(s). \n", num);
+
+    for (int i = 0; i < num; i++)
+    {
+        tid[i] = i;
+        pthread_create(&thread[i], NULL, thread_Fun, (void *)&tid[i]);
+    }
+
+    for (int i = 0; i < num; i++)
+    {
+        pthread_join(thread[i], NULL);
+    }
+    return 0;
+}
