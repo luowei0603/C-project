8d035eb614f33596bd03d9fb9e3e2cf4804fb8dd
luowei3 <1106581243@qq.com>
2021-05-20 10:27:56 +0800

    自定义assert

---
+++ test_assert/test_assert.cpp
@@ -0,0 +1,37 @@
+#include <assert.h>
+#include <stdio.h>
+
+#include <iostream>
+using namespace std;
+
+#ifdef AC_ENABLE_ASSERT
+#define AC_ASSERT(expr)                    \
+  do {                                     \
+    if (!(expr)) {                         \
+      printf("assert failed:%s\n", #expr); \
+      assert(0);                           \
+    }                                      \
+  } while (0)
+#else
+#define AC_ASSERT(expr)                    \
+  do {                                     \
+    if (!(expr)) {                         \
+      printf("assert failed:%s\n", #expr); \
+    }                                      \
+  } while (0)
+#endif
+
+bool test(int a) {
+  std::cout << "this is test, input = " << a << std::endl;
+  if (a > 0) return true;
+  return false;
+}
+
+int main() {
+  int a = -5;
+  int b = 5;
+  AC_ASSERT(test(a));
+  // printf("hello\n");
+  AC_ASSERT(test(b));
+  return 0;
+}
